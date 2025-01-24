bd92fa1fe3b8f9db1977716cde748e2d52d42fc9
Mason Storms <billowillo@users.noreply.github.com>
2025-01-24 16:12:49 -0700

    Updated CSS
    Removed template css file, added Counter.css.
    
    provides dark/purple theme centered to page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-
+import './Counter.css';
 function Counter() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <div className="counter-container">
       <h1>Counter Hello</h1>
       <p>Count: {count}</p>
       <button onClick={() => setCount(count + 1)}>Increment</button>
